48564c7d488456197f2c770cb21b8b59ec8d0b96
Natalia <nivanova86@bk.ru>
2022-12-27 18:08:11 +0300

    add task4 to HW4

--- HW_4/randint_list.py
+++ HW_4/randint_list.py
@@ -1,1 +1,8 @@
+from random import randint
 
+def random_list(size, min, max):
+    r_list = []
+    for i in range(size):
+        r_list.append(randint(min, max))   
+
+    return r_list
--- HW_4/task4.py
+++ HW_4/task4.py
@@ -6,5 +6,19 @@
 # Вывод:
 # 2 1
 
-from random import randint
+import randint_list
 
+num_list = randint_list.random_list(8, 0, 6)
+print('list of random natural numbers: \n', num_list)
+
+repeats_set = set()
+for num in num_list:
+    if num_list.count(num) > 1:
+        repeats_set.add(num)
+
+new_num_list = []
+for num in num_list:
+    if num not in repeats_set:
+        new_num_list.append(num)
+print('list of non-repeating elements of the original list: \n', new_num_list)
+    
